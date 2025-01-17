48e20fa09f25e2f0e13d34adbe9a0deac4618d36
stan5370 <tanshannon1@outlook.com>
2025-01-16 20:12:09 -0600

    IT WORKS

---
+++ sc8_driver_controls/Debug/subdir_vars.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../can.c \
+../gauge.c \
+../pedal.c \
+../tri86.c \
+../usci.c 
+
+C_DEPS += \
+./can.d \
+./gauge.d \
+./pedal.d \
+./tri86.d \
+./usci.d 
+
+OBJS += \
+./can.o \
+./gauge.o \
+./pedal.o \
+./tri86.o \
+./usci.o 
+
+OBJS__QUOTED += \
+"can.o" \
+"gauge.o" \
+"pedal.o" \
+"tri86.o" \
+"usci.o" 
+
+C_DEPS__QUOTED += \
+"can.d" \
+"gauge.d" \
+"pedal.d" \
+"tri86.d" \
+"usci.d" 
+
+C_SRCS__QUOTED += \
+"../can.c" \
+"../gauge.c" \
+"../pedal.c" \
+"../tri86.c" \
+"../usci.c" 
+
+
